5eff98854deea3cfe9ddb9c150ef00b6e81cbd3c
Alvaro <ahdezex@gmail.com>
2023-11-15 20:39:32 +0100

    creating an AST for the HTML

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,5 +1,11 @@
 import { readFileSync } from 'fs';
 
+interface Node {
+	type: string;
+	value?: string;
+	children?: Node[];
+}
+
 export default class Parser {
 	private path: string;
 	private content: string = '';
@@ -32,7 +38,7 @@ export default class Parser {
 
 		const html = this.content.slice(start, end + 7);
 
-		return html;
+		return this.parseHTML(html);
 	}
 
 	readStyles() {
@@ -42,5 +48,75 @@ export default class Parser {
 		const styles = this.content.slice(start, end + 8);
 
 		return styles;
+	}
+
+	parseHTML(html: string): Node[] {
+		function getNextNode(index: number) {
+			const startTagIndex = html.indexOf('<', index);
+			if (startTagIndex === -1) {
+				return { index: html.length };
+			}
+
+			const endTagIndex = html.indexOf('>', startTagIndex);
+			if (endTagIndex === -1) {
+				throw new Error('Tag no cerrado');
+			}
+
+			const tagContent = html.substring(startTagIndex + 1, endTagIndex);
+			if (tagContent[0] === '/') {
+				return {
+					type: 'closing',
+					tagName: tagContent.slice(1),
+					index: endTagIndex + 1,
+				};
+			}
+
+			const tagEnd = tagContent.indexOf(' ');
+			const tagName =
+				tagEnd === -1 ? tagContent : tagContent.substring(0, tagEnd);
+			return { type: 'opening', tagName, index: endTagIndex + 1 };
+		}
+
+		function parse(
+			index = 0,
+			closingTag = ''
+		): { nodes: Node[]; index: number } {
+			const nodes: Node[] = [];
+
+			while (index < html.length) {
+				const nextNode = getNextNode(index);
+
+				if (
+					nextNode.index === html.length ||
+					(nextNode.type === 'closing' && nextNode.tagName === closingTag)
+				) {
+					if (index < nextNode.index - closingTag.length - 3) {
+						const textContent = html
+							.substring(index, nextNode.index - closingTag.length - 3)
+							.trim();
+						if (textContent) {
+							nodes.push({ type: 'text', value: textContent });
+						}
+					}
+					return { nodes, index: nextNode.index };
+				}
+
+				if (nextNode.type === 'opening') {
+					const childNodes = parse(nextNode.index, nextNode.tagName);
+					nodes.push({ type: nextNode.tagName, children: childNodes.nodes });
+					index = childNodes.index;
+				} else {
+					const textContent = html.substring(index, nextNode.index).trim();
+					if (textContent) {
+						nodes.push({ type: 'text', value: textContent });
+					}
+					index = nextNode.index;
+				}
+			}
+
+			return { nodes, index };
+		}
+
+		return parse().nodes;
 	}
 }
--- tests/parser/read.test.ts
+++ tests/parser/read.test.ts
@@ -2,13 +2,31 @@ import { describe, expect, it } from 'vitest';
 import Parser from '../../src/parser';
 
 describe('read', () => {
-	it('should read a file', () => {
+	it('should parse HTML', () => {
 		const current = process.cwd();
 		const path = `${current}/tests/parser/test.latte`;
 
 		const parser = new Parser(path);
 		parser.read();
 
-		expect(parser.readScript()).toBe('<script>let a = 1;</script>');
+		const ast = parser.readHTML();
+
+		const want = {
+			type: 'html',
+			children: [
+				{ type: 'span', children: [{ type: 'text', value: 'hello' }] },
+				{
+					type: 'div',
+					children: [
+						{
+							type: 'span',
+							children: [{ type: 'text', value: 'world' }],
+						},
+					],
+				},
+			],
+		};
+
+		expect(ast[0]).toEqual(want);
 	});
 });
